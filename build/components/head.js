b3ccfaa94a32ab21d05b703baab4a8c9243036fc
Pranav Malewadkar <pranav@Pranavs-MacBook-Air.local>
2020-12-09 14:40:26 -0800

    feat: add basic typescript setup

---
+++ build/components/head.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Head = void 0;
+var Head = /** @class */ (function () {
+    function Head() {
+    }
+    return Head;
+}());
+exports.Head = Head;
---
+++ build/components/html.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Html = void 0;
+var Html = /** @class */ (function () {
+    function Html() {
+    }
+    return Html;
+}());
+exports.Html = Html;
